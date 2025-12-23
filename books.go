48b86306a141363d3f9ccd69324d3c121f433f13
Sarvagya035 <sarvagya035@gmail.com>
2025-12-24 00:41:37 +0530

    Get all functions

--- books.go
+++ books.go
@@ -1,1 +1,80 @@
 package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+)
+
+func checkerr(err error) {
+
+	if err != nil {
+		fmt.Println("Error happened: ", err)
+		os.Exit(1)
+	}
+}
+
+// func to get all the books.
+func getbooks() []Book {
+
+	booksbyte, err := os.ReadFile("./books.json")
+
+	checkerr(err)
+
+	var books []Book
+
+	err = json.Unmarshal(booksbyte, &books)
+
+	return books
+}
+
+func HandleallBooks(getCmd *flag.FlagSet, all *bool, id *string) {
+
+	getCmd.Parse(os.Args[2:])
+
+	// Checking for id or string
+
+	if !*all && *id == "" {
+
+		fmt.Println("subcommand --all or --id needed")
+		getCmd.PrintDefaults()
+		os.Exit(1)
+	}
+
+	// if for all books return all books
+
+	if *all {
+
+		books := getbooks()
+
+		fmt.Printf("Id \t Title \t Author \t Price \t ImageURL \n")
+
+		for _, book := range books {
+
+			fmt.Printf("%v \t %v \t %v \t %v \t %v \n", book.ID, book.Title, book.Author, book.Price, book.ImageUrl)
+		}
+	}
+
+	if *id != "" {
+
+		books := getbooks()
+		fmt.Printf("Id \t Title \t Author \t Price \t ImageURL \n")
+
+		// to check a book exist or not
+
+		var foundBook bool
+		for _, book := range books {
+			foundBook = true
+			if *id == book.ID {
+				fmt.Printf("%v \t %v \t %v \t %v \t %v \n", book.ID, book.Title, book.Author, book.Price, book.ImageUrl)
+			}
+		}
+
+		if !foundBook {
+			fmt.Println("Book not found")
+			os.Exit(1)
+		}
+	}
+
+}
--- main.go
+++ main.go
@@ -1,9 +1,41 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
 type Book struct {
 	ID       string `json:"id"`
 	Title    string `json:"title"`
 	Author   string `json:"author"`
 	Price    string `json:"price"`
 	ImageUrl string `json:"imageurl"`
+}
+
+func main() {
+
+	/*
+		get books --id or --all
+	*/
+
+	getCmd := flag.NewFlagSet("get", flag.ExitOnError)
+	getall := getCmd.Bool("all", false, "List all the books")
+	getid := getCmd.String("id", "", "List book by particular Id")
+
+	if len(os.Args) < 2 {
+		fmt.Println("expected 'get' subcommand")
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+
+	case "get":
+		HandleallBooks(getCmd, getall, getid)
+	default:
+		fmt.Println("Please provide get, update, update, delete commands")
+		os.Exit(1)
+	}
+
 }
